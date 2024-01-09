001de3e1ce3dc424692aca782a309c7e55ec6e33
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2024-01-10 00:43:49 +0330

    new commit

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'core'
 urlpatterns = [
-    path('', views.Index.as_view(), name='index')
+    path('', views.index, name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
 
-class Index(TemplateView):
-    template_name = 'core/index.html'
+# class Index(TemplateView):
+#     template_name = 'core/index.html'
+
+def index(request: HttpRequest):
+    return render(request, 'core/index.html')
