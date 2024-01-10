1c7e48710b965ac406548102976f50b7a59a44da
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2024-01-10 21:01:12 +0330

    skills validator

--- Skills/models.py
+++ Skills/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Skill(models.Model):
     title = models.CharField(max_length=120)
-    percent = models.IntegerField()
+    percent = models.IntegerField(validators=[MaxValueValidator(100, 'It is wrong!'),
+                                              MinValueValidator(0, 'It is wrong!')])
 
     def __str__(self):
         return self.title
