b74f885dac4d979ce0cbbed1bf4aa9d8dd483372
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2024-01-15 19:25:46 +0330

    CkEditor

--- Skills/models.py
+++ Skills/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from .validators import max_value_validator, min_value_validator
-
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Skill(models.Model):
     title = models.CharField(max_length=120)
@@ -14,7 +15,8 @@ class Skill(models.Model):
 
 class Biography(models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField(max_length=1000)
+    # description = RichTextUploadingField(max_length=2000)
+    description = models.TextField(max_length=2000)
     image = models.ImageField(max_length=1000, upload_to='media/images/biography')
     is_active = models.BooleanField()
 
