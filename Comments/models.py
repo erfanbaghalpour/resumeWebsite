ac62e0fd13b9b849501b532cad7e73b5ceecb659
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2024-02-01 23:31:21 +0330

    blog comment 2

--- Comments/models.py
+++ Comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Blog.models import Blog
 
-# Create your models here.
+
+class Comment(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=500)
+    text = models.TextField(max_length=5000)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
