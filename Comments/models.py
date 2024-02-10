bc56c720f3b8304cfe377bdcc047d238e4fd0fc0
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2024-02-10 13:20:50 +0330

    blog comment 6

--- Comments/models.py
+++ Comments/models.py
@@ -15,14 +15,4 @@ class Comment(models.Model):
         return self.name
         # return f"{self.id}, {self.blog.title}"
 
-# class Reply(models.Model):
-#     name = models.CharField(max_length=200)
-#     email = models.EmailField(max_length=500)
-#     re_comment = models.TextField(max_length=5000)
-#     created = models.DateTimeField(auto_now_add=True)
-#     active = models.BooleanField(default=False)
-#     # blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True, blank=True)
-#     comment_org = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True, related_name="recomment")
-#
-#     def __str__(self):
-#         return f"{self.comment_org.id}, {self.comment_org.blog.title}"
+
